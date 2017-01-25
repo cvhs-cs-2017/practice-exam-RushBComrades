e284bc16d688e6b8c1060a89d02d6830538fb13b
RushBComrades <hawkboy1111@yahoo.com>
2017-01-25 10:49:48 -0800

    loops done

--- Loops.py
+++ Loops.py
@@ -1,2 +1,13 @@
 """Use a loop to make a turtle draw a shape that is has at least 100 sides and
-that shows symmetry.  The entire shape must fit inside the screen""" 
+that shows symmetry.  The entire shape must fit inside the screen"""
+import turtle
+Vex = turtle.Turtle()
+Vex.speed(0)
+Vex.up()
+Vex.goto(0,150)
+Vex.down()
+for x in range (100):
+        Vex.forward(15)
+        Vex.right((360/100))
+print("done")
+input()
