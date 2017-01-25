6408568adc92ac6a0824a966c87b90c2200168f5
RushBComrades <hawkboy1111@yahoo.com>
2017-01-25 11:11:53 -0800

    EnCrip done

--- Encrypt.py
+++ Encrypt.py
@@ -1,2 +1,11 @@
 """Write a code that will take (AnyString) and substitute all vowels with
 character's like $#%^&* etc..... but DO NOT USE ' or " as any of your characters."""
+def void(o):
+    v = "aeiouAEIOU"
+    w = ""
+    for x in o:
+        if x in v:
+            x = "#"
+        w += x
+    return w
+print(void("AP"))
